29140b08d4eba29a7e637557fcdadeafc8ff3253
masishta <masishta@gmail.com>
2016-02-10 21:39:15 +0330

    Add Paarsig information variables and delete old methods

--- app/src/main/java/masishta/parsig/DatabaseHandler.java
+++ app/src/main/java/masishta/parsig/DatabaseHandler.java
@@ -1,6 +1,5 @@
 package masishta.parsig;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -14,120 +13,59 @@ import java.util.List;
  */
 public class DatabaseHandler extends SQLiteOpenHelper
 {
-    // Static Variables
-    // Database Version
-    private static final int DATABASE_VERSION = 1;
+  // Database Version
+  static final int DATABASE_VERSION = 1;
 
-    // Database Name
-    private static final String DATABASE_NAME = "mosayeb";
+  // Database Name
+  static final String DATABASE_NAME = "paarsig";
 
-    // Table Name
-    private static final String TABLE_INFO = "info";
+  // Database Path
+  String DATABASE_PATH;
 
-    // Info Table Columns Names
-    private static final String KEY_ID = "id";
-    private static final String KEY_NAME = "name";
-    private static final String Key_DESCRIPTION = "description";
+  // Table Name
+  final String TABLE_INFO = "parsik_senses";
 
-    public DatabaseHandler(Context context)
-    {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+  // Parsig Table Columns Names
+  private static final String _ID          = "ID";
+  private static final String _PERSIAN     = "name";
+  private static final String _DESCRIPTION = "desc";
+  private static final String _ENGLISH     = "en";
+  private static final String _DEUTSCH     = "de";
+  private static final String _FRENCH      = "fr";
+  private static final String _REFERENCE   = "refs";
 
-    // Creating Table
-    @Override
-    public void onCreate(SQLiteDatabase db)
-    {
-        String CREATE_INFO_TABLE = "CREATE TABLE " + TABLE_INFO + "("
-                + KEY_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," + KEY_NAME + " TEXT,"
-                + Key_DESCRIPTION + " TEXT " + ");";
 
-        db.execSQL(CREATE_INFO_TABLE);
-    }
+  public DatabaseHandler (Context context)
+  {
+    super (context, DATABASE_NAME, null, DATABASE_VERSION);
+    DATABASE_PATH = context.getDatabasePath (DATABASE_NAME).toString ();
+  }
 
-    // Upgrading Database
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-    {
-        // Drop older table if exist
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_INFO);
+  // Creating Table
+  @Override
+  public void onCreate (SQLiteDatabase db)
+  {
 
-        // Create table again
-        onCreate(db);
-    }
+  }
 
-    // Database Create, Read, Update, Delete Operations
-    // Adding new info
-    void addInfo(Info info)
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
+  // Upgrading Database
+  @Override
+  public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion)
+  {
 
-        ContentValues values = new ContentValues();
-        values.put(KEY_NAME, info.getName());
-        values.put(Key_DESCRIPTION, info.getDescription());
+  }
 
-        // Inserting Row
-        db.insert(TABLE_INFO, null, values);
-        db.close();
-    }
+  // Return Search Result
+  public List<ParsigInfoHolder> searchResult ()
+  {
+    List<ParsigInfoHolder> infoList = new ArrayList<ParsigInfoHolder> ();
 
-    // Reading single info
-    Info getInfo(int id)
-    {
-        SQLiteDatabase db = this.getReadableDatabase();
+    // Select All Query
+    String selectQuery = "SELECT * FROM " + TABLE_INFO + ";";
 
-        Cursor cursor = db.query(TABLE_INFO, new String[] {KEY_ID, KEY_NAME, Key_DESCRIPTION},
-                KEY_ID + "=?", new String[]{String.valueOf(id)}, null, null, null, null);
-        if (cursor != null)
-            cursor.moveToFirst();
-        Info info = new Info(cursor.getInt(0),
-                cursor.getString(1), cursor.getString(2));
+    SQLiteDatabase db = this.getWritableDatabase ();
+    Cursor cursor = db.rawQuery (selectQuery, null);
 
-        return info;
-    }
-
-    // Reading All Info
-    public List<Info> getAllInfo()
-    {
-        List<Info> infoList = new ArrayList<Info>();
-
-        // Select All Query
-        String selectQuery = "SELECT * FROM " + TABLE_INFO + ";";
-
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
-
-        // Loop through all rows and adding to list
-        if (cursor.moveToFirst())
-        {
-            do
-            {
-                Info info = new Info();
-                info.setID(cursor.getInt(0));
-                info.setName(cursor.getString(1));
-                info.setDescription(cursor.getString(2));
-
-                // Adding info to list
-                infoList.add(info);
-            } while (cursor.moveToNext());
-        }
-
-        return infoList;
-    }
-
-    // Updating single info
-    public int updateInfo(Info info)
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(KEY_NAME, info.getName());
-        values.put(Key_DESCRIPTION, info.getDescription());
-
-        // Updating row
-        return db.update(TABLE_INFO, values, KEY_ID + " =?", new String[]{
-                String.valueOf(info.getID())});
-    }
-
-    //
+    return infoList;
+  }
 }
